52e86b326d8a9ebebef523e640923bf6ba01cd05
lancewburklin <1677@holbertonschool.com>
2021-01-08 10:42:04 -0600

    Cut down on complexity

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -7,8 +7,15 @@ Finding the minimum operations
 
 def minOperations(n):
     res = 0
-    for i in range(2, n+1):
-        while n % i == 0:
-            res += i
-            n = n/i
+    i = 0
+    while i < n:
+    	if n % 2 == 0:
+    		n = n // 2
+    		res += 2
+    	else:
+    		break
+    for x in range(3, n+1, 2):
+        while n % x == 0:
+            res += x
+            n = n // x
     return res
