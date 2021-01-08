57080d1d603ec72f63fea4174ce5bc9cdacfdfb4
lancewburklin <1677@holbertonschool.com>
2021-01-08 10:43:15 -0600

    Fixed pep8 issues

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -9,11 +9,11 @@ def minOperations(n):
     res = 0
     i = 0
     while i < n:
-    	if n % 2 == 0:
-    		n = n // 2
-    		res += 2
-    	else:
-    		break
+        if n % 2 == 0:
+            n = n // 2
+            res += 2
+        else:
+            break
     for x in range(3, n+1, 2):
         while n % x == 0:
             res += x
