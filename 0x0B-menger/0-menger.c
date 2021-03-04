5056ce3dd2c40fa9e932b017ad738749f9b37551
lancewburklin <1677@holbertonschool.com>
2021-03-04 12:41:21 -0600

    Creating a menger sponge

---
+++ 0x0B-menger/0-menger.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+/**
+ * menger - Create a menger sponge
+ * @level: Level of the sponge
+ *
+ * Return: VOID
+**/
+
+void menger(int level)
+{
+	int i, j, size, d;
+	int depth = level;
+
+	size = pow(3, depth);
+	printf("Size: %d\n", size);
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			for (d = size / 3; d; d /= 3)
+			{
+				if ((i % (d * 3)) / d == 1 && (j % (d * 3)) / d == 1)
+					break;
+					printf(d ? " " : "#");
+			}
+				}
+				printf("\n");
+			}
+}
