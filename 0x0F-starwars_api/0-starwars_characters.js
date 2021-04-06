5d73f351395c295fb59867bf9dd2170fd11da922
lancewburklin <1677@holbertonschool.com>
2021-04-06 15:59:42 -0500

    Initial commit

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const chars = JSON.parse(body).characters;
+    for (let n = 0; n < chars.length; n++) {
+      request(chars[n], function (error, response, body) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
+  }
+});
---
+++ 0x0F-starwars_api/README.md
@@ -0,0 +1,1 @@
+Getting character names from star wars films
