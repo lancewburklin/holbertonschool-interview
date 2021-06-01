8aaf3b564872927dac53923a815b49fda815f544
lancewburklin <1677@holbertonschool.com>
2021-06-01 13:32:20 -0500

    Fixed linter issues

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -6,29 +6,31 @@
 /**
  * find_listint_loop - Find the loop in a list
  * @head: Head of the list
- * 
+ *
  * Return: The loop node, or NULL for no loop
  */
 
 listint_t *find_listint_loop(listint_t *head)
 {
-    listint_t *turtle = head;
-    listint_t *hare = head;
-    if (head == NULL)
-        return (NULL);
-    while (turtle->next != NULL && hare->next->next != NULL)
-    {
-        hare = hare->next->next;
-        turtle = turtle->next;
-        if (hare == turtle)
-            break;
-    }
-    if (turtle != hare)
-        return (NULL);
-    turtle = head;
-    while (turtle != hare) {
-        turtle = turtle->next;
-        hare = hare->next;
-    }
-    return (hare);
+	listint_t *turtle = head;
+	listint_t *hare = head;
+
+	if (head == NULL)
+		return (NULL);
+	while (turtle->next != NULL && hare->next->next != NULL)
+	{
+		hare = hare->next->next;
+		turtle = turtle->next;
+		if (hare == turtle)
+			break;
+	}
+	if (turtle != hare)
+		return (NULL);
+	turtle = head;
+	while (turtle != hare)
+	{
+		turtle = turtle->next;
+		hare = hare->next;
+	}
+	return (hare);
 }
