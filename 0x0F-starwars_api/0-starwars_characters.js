724aa0581ff2b9b93f618397639474e741cbe513
lancewburklin <1677@holbertonschool.com>
2021-06-16 19:58:46 -0500

    Made character calls async

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -2,19 +2,26 @@
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-request(url, function (error, response, body) {
+function getChatacter (character) {
+  return new Promise((resolve, reject) => {
+    request(character, function (error, response, body) {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(body).name);
+      }
+    });
+  });
+}
+
+request(url, async function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
     const chars = JSON.parse(body).characters;
     for (let n = 0; n < chars.length; n++) {
-      request(chars[n], function (error, response, body) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
-      });
+      const character = await getChatacter(chars[n]);
+      console.log(character);
     }
   }
 });
