ece6dd08250b51c35070f0a534538e464480c50d
lancewburklin <1677@holbertonschool.com>
2021-06-17 01:54:49 -0500

    Removed errors

--- 0x18-merge_sort/0-merge_sort.c
+++ 0x18-merge_sort/0-merge_sort.c
@@ -12,74 +12,81 @@
 
 void merge_sort(int *array, size_t size)
 {
-	recur_merge_sort(array, 0, size - 1);
+	int *array2;
+
+	if (size < 2)
+		return;
+	array2 = malloc(sizeof(int) * size);
+	recur_merge_sort(array, array2, 0, size - 1);
+	free(array2);
 }
 
 /**
  * recur_merge_sort - Recursive merge call
  * @array: Array to use
+ * @array2: Second array
  * @left: Left side of array
  * @right: Right side of array
 */
 
-void recur_merge_sort(int *array, size_t left, size_t right)
+void recur_merge_sort(int *array, int *array2, size_t left, size_t right)
 {
 	size_t mid = (left + (right - left - 1) / 2);
 
 	if (left < right)
 	{
-		recur_merge_sort(array, left, mid);
-		recur_merge_sort(array, mid + 1, right);
+		recur_merge_sort(array, array2, left, mid);
+		recur_merge_sort(array, array2, mid + 1, right);
 		if (left - right != 0)
-			merge(array, left, mid, right);
+			merge(array, array2, left, mid, right);
 	}
 }
 
 /**
  * merge - Merge two arrays
  * @array: Array to merge
+ * @array2: Second array
  * @l: Left side
  * @m: Middle
  * @r: right end point
 */
 
-void merge(int *array, size_t l, size_t m, size_t r)
+void merge(int *array, int *array2, size_t l, size_t m, size_t r)
 {
 	size_t i, j = 0, k = l, n1  = m - l + 1, n2 = r - m;
-	int L[n1], R[n2];
 
 	printf("Merging...\n[left]: ");
 	print_sub_array(array, l, m);
 	printf("[right]: ");
 	print_sub_array(array, m, r);
 	for (i = 0; i < n1; i++)
-		L[i] = array[l + i];
-	for (i = 0; i < n2; i++)
-		R[i] = array[m + 1 + i];
-	i = 0;
-	while (i < n1 && j < n2)
+		array2[i] = array[l + i];
+	for (j = i; j - i < n2; j++)
+		array2[j] = array[m + 1 + (j - i)];
+	j = i, i = 0;
+	while (i < n1 && j - n1 < n2)
 	{
-		if (L[i] <= R[j])
+		if (array2[i] <= array2[j])
 		{
-			array[k] = L[i];
+			array[k] = array2[i];
 			i++;
 		}
 		else
 		{
-			array[k] = R[j];
+			array[k] = array2[j];
 			j++;
 		}
 		k++;
 	}
 	while (i < n1)
 	{
-		array[k] = L[i];
+		array[k] = array2[i];
 		i++;
 		k++;
 	}
 	while (j < n2)
 	{
-		array[k] = R[j];
+		array[k] = array2[j];
 		j++;
 		k++;
 	}
