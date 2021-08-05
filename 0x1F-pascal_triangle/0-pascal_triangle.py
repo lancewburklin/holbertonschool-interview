590cbd335cc00a02a7fbe3b2c0e8faba0f1b51ca
lancewburklin <1677@holbertonschool.com>
2021-08-05 14:12:52 -0500

    Pascal triangle calculator

---
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    triangle = []
+    if (n <= 0):
+        return []
+    for row in range(1, n + 1):
+        num = 1
+        newRow = []
+        for i in range(1, row + 1):
+            newRow.append(num)
+            num = int(num * (n - i) / i)
+        triangle.append(newRow)
+    return (triangle)
---
+++ 0x1F-pascal_triangle/README.md
@@ -0,0 +1,1 @@
+Find Pascal's Triangle of n
