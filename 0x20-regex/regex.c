186ecb93804fc6a79fe0b03ad8df95a978e92960
lancewburklin <1677@holbertonschool.com>
2021-08-12 14:21:47 -0500

    Calling compare function

--- 0x20-regex/regex.c
+++ 0x20-regex/regex.c
@@ -21,7 +21,7 @@ int regex_match(char const *str, char const *pattern)
 		return (regex_match(str + 1, pattern + 1));
 	if (*pattern == '*')
 		return (regex_match(str, pattern + 1) || regex_match(str + 1, pattern));
-	return (0);
+	return (compare());
 }
 
 /**
